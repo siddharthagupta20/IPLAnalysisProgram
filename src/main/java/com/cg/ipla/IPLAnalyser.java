dd346f33dc380eca36df76f64d73e0e126d34707
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-29 02:45:32 +0530

    [Siddhartha] refactored:getting batsman with best avg

--- src/main/java/com/cg/ipla/IPLAnalyser.java
+++ src/main/java/com/cg/ipla/IPLAnalyser.java
@@ -1,17 +1,23 @@
 package com.cg.ipla;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.cg.ipla.dto.BattingData;
 import com.cg.ipla.dto.BowlingData;
 
 public class IPLAnalyser {
 
+	public ArrayList<BowlingData> bowlersList = null;
+	public ArrayList<BattingData> batsmenList = null;
+
 	public List<BowlingData> getBowlingDataFile(String bowlingDataPath) {
 		try {
-			List<BowlingData> bowlersList=new CSVFileLoader().loadCsvFile(bowlingDataPath, BowlingData.class);
-			bowlersList.remove(bowlersList.size()-1);
-			bowlersList.remove(bowlersList.size()-1);
+			bowlersList = (ArrayList<BowlingData>) new CSVFileLoader().loadCsvFile(bowlingDataPath, BowlingData.class);
+			bowlersList.remove(bowlersList.size() - 1);
+			bowlersList.remove(bowlersList.size() - 1);
 			return bowlersList;
 		} catch (IPLException e) {
 			System.out.println(e.getMessage());
@@ -22,12 +28,18 @@ public class IPLAnalyser {
 
 	public List<BattingData> getBattingDataFile(String battingDataPath) {
 		try {
-			return (new CSVFileLoader()).loadCsvFile(battingDataPath, BattingData.class);
+			batsmenList = (ArrayList<BattingData>) (new CSVFileLoader()).loadCsvFile(battingDataPath, BattingData.class);
+			return batsmenList;
 		} catch (IPLException e) {
 			System.out.println(e.getMessage());
 		}
 		return null;
 
+	}
+
+	public List<BattingData> sortingBatsmanWithBestAverage() {
+		Comparator<BattingData> comparatorBestAvg=Comparator.comparing(BattingData::getAverage);
+		return batsmenList.stream().sorted(comparatorBestAvg).collect(Collectors.toList());
 	}
 
 	public <E> void printData(List<E> listToPrint) {
