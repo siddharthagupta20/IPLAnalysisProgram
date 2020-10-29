7daaa6750e89f9e5ba80fe7aa0b7a82a5a34ca48
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-29 20:57:55 +0530

    [Siddhartha] refactored: retrieved bowlers with best economy

--- src/main/java/com/cg/ipla/BowlingSortBy.java
+++ src/main/java/com/cg/ipla/BowlingSortBy.java
@@ -6,19 +6,18 @@ import com.cg.ipla.dto.BowlingData;
 
 public class BowlingSortBy {
 
-	
-	
-	public static final Comparator<BowlingData> BEST_AVG=Comparator.comparing(BowlingData::getAverage);
-	
-	public static final Comparator<BowlingData> BEST_STRIKE_RATE=Comparator.comparing(BowlingData::getStrikeRate);
-	
-	
+	public static final Comparator<BowlingData> BEST_AVG = Comparator.comparing(BowlingData::getAverage);
+
+	public static final Comparator<BowlingData> BEST_STRIKE_RATE = Comparator.comparing(BowlingData::getStrikeRate);
+
+	public static final Comparator<BowlingData> BEST_ECONOMY = Comparator.comparing(BowlingData::getEconomy);
+
 	public static Comparator<BowlingData> addConditionsInOrder(Comparator<BowlingData>[] comparators) {
-		if(comparators.length==0)
+		if (comparators.length == 0)
 			return null;
-		Comparator<BowlingData> temp=comparators[0];
-		for(int i=1;i<comparators.length;i++) {
-			temp=temp.thenComparing(comparators[i]);
+		Comparator<BowlingData> temp = comparators[0];
+		for (int i = 1; i < comparators.length; i++) {
+			temp = temp.thenComparing(comparators[i]);
 		}
 		return temp;
 	}
